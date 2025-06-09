7ab5f867394d4e996783fa444ddc9c65b68b328b
AslanMuskSol <aslanmusk.sol@gmail.com>
2025-06-09 21:42:42 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from discord.ext import commands, tasks
 from keep_alive import keep_alive
 
 # ======================== CONFIG SECTION ========================
-TOKEN = "MTM4MTU0MjA2MzA5ODY5MTYxNQ.GMQ0Dd.LzFLSJO9AsW2bfuJH9hBuo3lJ9Zg9ozSyHFQfA"  # Replace with your bot token
+TOKEN = ""  # Replace with your bot token
 GUILD_ID = 1245617853311356989  # Replace with your server ID
 CHANNEL_ID = 1381538971430944778  # The channel where trigger messages are monitored
 ROLE_NAME = "UM"  # Role to give and auto-remove
